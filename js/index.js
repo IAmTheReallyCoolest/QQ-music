bc734de32e21fe747874f9d2a3683fd6478af5af
IAmTheReallyCoolest <39411225+IAmTheReallyCoolest@users.noreply.github.com>
2018-10-15 15:42:00 +0800

    10.15.1

--- js/index.js
+++ js/index.js
@@ -51,7 +51,57 @@ $(function(){
 		};
 		count=num;
 		num++;
-		
+		$ul1.animate({left:-num*1200},200)
+		$ol1li.eq(num==4?0:num).css("background","#fff").siblings().css("backgroundColor","#6c6c6c");
+	})
+	//点击banner 左侧按钮的事件
+	$("#banner .anniul").click(function(){
+		if(num<=0){
+			$ul1.css("left",-4800);
+			num=4;
+		};
+		count=num;
+		num--;
+		$ul1.animate({left:-num*1200},200)
+		$ol1li.eq(num==4?0:num).css("backgroundColor","#fff").siblings().css("backgroundColor","#6c6c6c");
+	})
+	var count;
+	$ol1li.click(function(){
+		$(this).css("backgroundColor","#fff").siblings().css("backgroundColor","#6c6c6c");
+		count=$(this).index();
+		num=count;
+		$ul1.animate({left:-count*1200},200)
+	})
+	//移入banner的li事件
+	var num1;
+	var $pic1=$("#banner .keshi1 .pic1");
+	var $lis=$("#banner .keshi1 li");
+	var $imgxia=$("#banner .keshi1 .imgxia");
+	var $bofang=$("#banner .keshi1 .bofang");
+	
+	$lis.mouseenter(function(){
+		num1=$(this).index();
+		$pic1.eq(num1).stop().css("opacity",0.9).animate({
+			width:320,
+			height:320,
+			left:-10,
+			top:-10
+		},200)
+		$imgxia.eq(num1).css("backgroundColor","#31c27c");
+		$bofang.eq(num1).stop().fadeIn(200);
+	})
+	//----移出banner的li事件
+	$lis.mouseleave(function(){
+		num1=$(this).index();
+		$bofang.stop().eq(num1).fadeOut(200);
+		$pic1.eq(num1).stop().css("opacity",1).animate({
+			width:300,
+			height:300,
+			left:0,
+			top:0
+		},200)
+		$imgxia.eq(num1).css("backgroundColor","rgba(0,0,0,0.7)");
+		$bofang.eq(num1).stop().fadeOut(200);
 	})
 });
 
