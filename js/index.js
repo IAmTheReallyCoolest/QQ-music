aaadc8749191fe914bb3b3ce7c781c00fce97917
IAmTheReallyCoolest <39411225+IAmTheReallyCoolest@users.noreply.github.com>
2018-10-22 21:18:26 +0800

    10.22

--- js/index.js
+++ js/index.js
@@ -233,105 +233,161 @@ var config = [
         }, //1
 
 ]; 
-console.log(config.length);
+
 //config其实就是一个配置单 规定了每张图片的大小位置层级透明度
 
-	//1获取元素
-	var anniuLeft = $("#jingCai .anniuil")[0];
-	var anniuRight = $("#jingCai .anniur")[0];
-	var jcbox=document.getElementById("jcbox");
+	//1 获取元素
+    var anniuLeft=$("#jingCai .anniul")[0];
+    var anniuRight=$("#jingCai .anniur")[0];
+    var jcbox=document.getElementById("jcbox");
     var slide=jcbox.children[0];//可视区
-	var list = slide.children[0]; //要运动的ul
-	var lis = list.children; //要运动的li
-	//2设置每个li的默认显示区域
-	changPos();
-	var flag=true;//节流阀的开关，值为true表示可以执行
-	//设置五个标签的自定义属性值
-	for(var i =0;i<lis.length;i++){
-		lis[i].flag=true;
-	}
-	
-	//右键点击操作
-	anniuLeft.onclick=function(){
-		if(flag){
-			//点击按钮后，只能执行一次效果，采用变量控制的方式
-			flag=false;
-			for(var i=0;i<lis.length;i++){
-				lis[i].flag=false;
-			}
-			//将数组config中的第一个元素删除掉，并添加到config的最后
-			config.push(config.shift());
-			//根据config中的新位置重新设置li的运动
-			changePos();
-		}
-	};
+    var list=slide.children[0];//要运动的ul
+    var lis=list.children;//要运动的li
 
-	function changePos(){
-		for(var i=0;i<config.length;i++){
-			animate(lis[i],config[i],function(){
-				//此处可以保证运动执行完毕
-				//由于此效果中每个原色的运动位置相近，所以再设置时进行简单的判断即可，不用进行精确的判断方式
-				flag=true;
-			});
-		
-		}
-	}
+    //2 设置每个li的默认显示位置
+    changePos();
 
-	function animate(element,datas,fn){
-		clearInterval(element.timer);
-		element.timer=setInterval(function(){
-			//1 设置假设条件：假设本轮定时器执行后可以进行清除
-			var flag=true;
-			//先对获取到的数据进行遍历，对每一组数据进行相同操作即可
-			for(var k in datas){
-				//针对透明度和层级进行单独处理
-				if(k=="opacity"){
-					var styleName=k;
-					var target=datas[k]*100;
-					//opacity是c3属性，使用getstyle获取后不会出现“auto”
-					//js中的小鼠进行加减计算会出现精度问题
-					var current =getStyle(element,styleName)*100;
-					var step=(target-current)/10;
-					step=step>0? Math.ceil(step):Math.floor(step);
-					current=current+step;
-					//设置透明度时，除以设置过的对应倍数
-					element.style[styleName]=current/100;
-					
-					if(current!=target){
-						flag=false;
-					}
-				}else if(k=="zIndex"){
-					//层级为整数值，所以不需要设置运动效果，直接设置为指定值即可
-					element.style.zIndex=datas[k];
-				}else{
-					//styleName实际上是k
-					//target:实际上是obj[k]
-					var styleName=k;
-					var target=datas[k];
-					//下面这段代码可以让某一个样式运动到指定的位置
-					var current=parseInt(getStyle(element,styleName))||0;
-					var step=(target-current)/10;
-					step=step>0?Math.ceil(step):Math.floor(step);
-					current=current+step;
-					element.style[styleName]=current+"px";
-					//此位置原来是书写清除定时器的代码位置，但是某个样式运动完毕并不能进行清除，其他样式可能没到，使用假设成立法解决
+
+    var flag = true;//节流阀的开关，值为true，表示可以执行
+    //设置五个标签的自定义属性值
+    for (var i = 0; i < lis.length; i++) {
+        lis[i].flag = true;
+    }
+
+    //4 右按钮点击操作
+    anniuLeft.onclick = function () {
+        if (flag) {
+            //点击按钮后，只能执行一次效果,采用变量控制的方式
+            flag = false;
+            for (var i = 0; i < lis.length; i++) {
+                lis[i].flag = false;
+            }
+
+
+            //将数组config中的第一个元素删除掉，并添加到config的最后
+            config.push(config.shift());
+
+            //根据config中的新位置，重新设置li的运动
+            changePos();
+        }
+
+    };
+
+    //5 左按钮点击操作
+    anniuRight.onclick = function () {
+        if (flag) {
+            flag = false;
+            //将数组的最后一个元素移动到数组最前面
+            config.unshift(config.pop());
+            changePos();
+        }
+    };
+
+    //---------点击jingCai 底部按钮  的事件
+    var $jcolli=$("#jcbox ol li ")
+    //var countem;
+    $jcolli.click(function(){
+        var i=$(this).index();
+        $(this).css("backgroundColor","#ccc").siblings().css("backgroundColor","#6c6c6c");
+        if (flag) {
+            flag = false;
+            //将数组的最后一个元素移动到数组最前面
+
+            var arr= config.splice(config.length-i,i);
+            console.log(arr);
+            for (var i=arr.length-1; i>=0; i--) {
+                config.unshift(arr[i]);
+            }
+         
+            changePos();
+        }
+
+    })
+
+
+
+    function changePos() {
+        for (var i = 0; i < config.length; i++) {
+            animate(lis[i], config[i], function () {
+                //此处可以保证运动执行完毕
+                //由于此效果中每个元素的运动位置相近，所以在设置时进行简单的判断即可，不用进行精确的判断方式
+                flag = true;
+            });
+        }
+    }
+
+    function animate(element, datas, fn) {
+
+        clearInterval(element.timer);
+        element.timer = setInterval(function () {
+
+            //1 设置假设条件：假设本轮定时器执行后可以进行清除
+            var flag = true;
+
+            //先对获取到的数据进行遍历，对每一组数据进行相同操作即可
+            for (var k in datas) {
+                //针对透明度和层级进行单独处理
+                if (k == "opacity") {
+                    var styleName = k;
+                    var target = datas[k] * 100;
+
+                    //opacity是c3属性，使用getStyle获取后不会出现"auto"
+                    //js中的小数进行加减计算会出现精度问题
+                    var current = getStyle(element, styleName) * 100;
+                    var step = (target - current) / 10;
+                    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                    current = current + step;
+                    //设置透明度时，除以设置过的对应倍数
+                    element.style[styleName] = current / 100;
+
+                    if (current != target) {
+                        flag = false;
+                    }
+
+                } else if (k == "zIndex") {
+                    //层级为整数值，所以不需要设置运动效果,直接设置为指定值即可
+                    element.style.zIndex = datas[k];
+                } else {
+                    //styleName： 实际上是k
+                    //target： 实际上是obj[k]
+                    var styleName = k;
+                    var target = datas[k];
+
+                    //下面这段代码可以让某一个样式运动到指定的位置
+                    var current = parseInt(getStyle(element, styleName)) || 0;
+                    var step = (target - current) / 10;
+                    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                    current = current + step;
+                    element.style[styleName] = current + "px";
+
+                    //此位置原来是书写清除定时器的代码位置，但是某个样式运动完毕并不能进行清除，其他样式可能没到，使用假设成立法解决
                     //2 找到假设失败的条件：如果某个样式运动时没有到达指定位置，阻止清除
-					if(current!=target){
-						flag=false;
-					}
-				}
-			}
-			//需要整体检测，只有每个样式都达到了指定位置才可以设置清除
-			//3验证flag的值
-			if(flag){
-				clearInterval(element.timer);
-				if(typeof fn=="function"){
-					fn();
-				}
-				
-			}
-		},5);
-	}
+                    if (current != target) {
+                        flag = false;
+                    }
+                }
+            }
+
+            //需要整体检测，只有每个样式都到达了指定位置才可以设置清除
+            //3 验证flag的值
+            if (flag) {
+                clearInterval(element.timer);
+                if (typeof fn == "function") {
+                    fn();
+                }
+            }
+
+        }, 5);
+    }
+    function getStyle(element, styleName) {
+        //currentStyle   getComputedStyle
+        if (element.currentStyle) {
+            return element.currentStyle[styleName];
+        } else {
+            return getComputedStyle(element, null)[styleName];
+        }
+    }
+
 
 
 
