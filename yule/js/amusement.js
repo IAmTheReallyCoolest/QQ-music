e56a28b5c27575a32606f2fb1641497969e594cb
IAmTheReallyCoolest <39411225+IAmTheReallyCoolest@users.noreply.github.com>
2018-10-30 21:42:53 +0800

    10.30.2

---
+++ yule/js/amusement.js
@@ -0,0 +1,23 @@
+//树叶飘落
+window.onload=function(){
+	var leafScene=function(el){
+		this.viewport=el;
+		this.world=document.createElement('div');
+		this.leaves=[];
+		this.options={
+			numLeaves:20,
+			wind:{
+			magnitude:1.2,
+			maxSpeed:12,
+			duration:300,
+			start:0,
+			speed:0
+		},
+			
+		};
+		this.width=this.viewport.offsetWidth;
+		this.height=this.viewport.offsetHeight;
+		//animation helper
+		
+	}
+}
